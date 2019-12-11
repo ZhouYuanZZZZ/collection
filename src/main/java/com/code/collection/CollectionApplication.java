59af9dbc2c48b6858bf571ae9d0be75fe9b1f93b
ZhouYuanZZZZ <zy1270157082@outlook.com>
2019-12-12 00:57:48 +0800

    init commit

---
+++ src/main/java/com/code/collection/CollectionApplication.java
@@ -0,0 +1,14 @@
+package com.code.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionApplication.class, args);
+    }
+
+    
+}
