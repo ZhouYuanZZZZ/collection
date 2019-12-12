abd8a7ef1ba3f6bc0fe87d8e59304e0f52244b73
ZhouYuanZZZZ <zy1270157082@outlook.com>
2019-12-13 00:22:00 +0800

    hdfs

---
+++ src/test/java/com/code/collection/hdfs/HDFSClientTest.java
@@ -0,0 +1,13 @@
+package com.code.collection.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.jupiter.api.Test;
+
+public class HDFSClientTest {
+
+    @Test
+    public void initHDFS(){
+
+        Configuration configuration = new Configuration();
+    }
+}
